81e983cc48cffe179c74b2391da9ee83cbb292c4
Stephen Merity <smerity@smerity.com>
2013-06-08 05:46:13 +1000

    Minor Fix: spelling correction in test description

--- static/js/angular_e2e.js
+++ static/js/angular_e2e.js
@@ -9,7 +9,7 @@ describe('RosettaFight E2E Test', function() {
     expect(element('h2').text()).toEqual('Rosetta Fight');
   });
 
-  it('should correct show task description', function() {
+  it('should correctly show task description', function() {
     select('taskSelector').option('100 doors');
     element('#taskSelectorArea button').click();
     expect(element('#taskDescription').text()).toContain('100 doors');
